841a4ee9ea200c858d4c57918345c1311587379e
CastorHunter <julien.parkinson@gmail.com>
2024-03-21 18:05:05 +0100

    box

--- Assets/Scripts/Porte1.cs
+++ Assets/Scripts/Porte1.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Porte1 : MonoBehaviour
 {
+    public BoxCollider2D bc2D;
     // Start is called before the first frame update
     void Start()
     {
