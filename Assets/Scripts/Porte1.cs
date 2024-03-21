d33b59bf7c7e1d4d1f5d4236f60665b48df6a836
CastorHunter <julien.parkinson@gmail.com>
2024-03-21 18:10:55 +0100

    tp1

--- Assets/Scripts/Porte1.cs
+++ Assets/Scripts/Porte1.cs
@@ -19,7 +19,7 @@ public class Porte1 : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.IsTouchingLayers(13))
+        if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(1);
         }
